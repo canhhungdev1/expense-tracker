8989c9b8c88c072a138d22cf99de2a32ca5ba600
PC Local Admin <Administrator@27535-4Y7V8S3.japan.gds.panasonic.com>
2026-04-07 16:27:18 +0700

    deploy

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api' // Khi deploy chung domain, hoặc điền URL Azure API của bạn vào đây
+  apiUrl: 'https://expense-tracker-nxfa.onrender.com/api'
 };
